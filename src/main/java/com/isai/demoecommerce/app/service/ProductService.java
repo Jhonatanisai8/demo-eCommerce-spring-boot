3e01fc78257edc277c7c6a0acb719d5133832026
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-05-06 00:23:50 -0500

    repositorio y clase servicio para la entidad producto

---
+++ src/main/java/com/isai/demoecommerce/app/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.isai.demoecommerce.app.service;
+
+import com.isai.demoecommerce.app.model.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductService {
+    Product saveProduct(Product product);
+
+    List<Product> getProducts();
+
+    Optional<Product> getProduct(Integer idProduct);
+
+    void updateProduct(Product product);
+
+    void deleteProduct(Integer idProduct);
+}
---
+++ src/main/java/com/isai/demoecommerce/app/service/impl/ProductServiceImpl.java
@@ -0,0 +1,43 @@
+package com.isai.demoecommerce.app.service.impl;
+
+import com.isai.demoecommerce.app.model.Product;
+import com.isai.demoecommerce.app.repository.ProductRepository;
+import com.isai.demoecommerce.app.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl
+        implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Override
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Optional<Product> getProduct(Integer idProduct) {
+        return productRepository.findById(idProduct);
+    }
+
+    @Override
+    public void updateProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    @Override
+    public void deleteProduct(Integer idProduct) {
+        productRepository.deleteById(idProduct);
+    }
+}
