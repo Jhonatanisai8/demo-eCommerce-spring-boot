b3cce0b8b9fbbb00affdae50098a303b2267069c
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-05-07 01:01:04 -0500

    controller para el producto y la vista de productos

---
+++ src/main/java/com/isai/demoecommerce/app/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.isai.demoecommerce.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/products")
+public class ProductController {
+
+    @GetMapping(path = "")
+    public String showProducts() {
+        return "products/show";
+    }
+}
