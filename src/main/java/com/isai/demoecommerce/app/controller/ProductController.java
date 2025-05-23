0605fdeb5dc1a4999bf4b80a30d4f14030482b27
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-05-23 12:21:15 -0500

    implementacion del metodo para editar un producto

--- src/main/java/com/isai/demoecommerce/app/controller/ProductController.java
+++ src/main/java/com/isai/demoecommerce/app/controller/ProductController.java
@@ -2,13 +2,14 @@ package com.isai.demoecommerce.app.controller;
 
 import com.isai.demoecommerce.app.model.Product;
 import com.isai.demoecommerce.app.model.User;
-import com.isai.demoecommerce.app.service.ProductService;
+import com.isai.demoecommerce.app.service.impl.ProductServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,7 +20,7 @@ public class ProductController {
 
     private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
 
-    private final ProductService productService;
+    private final ProductServiceImpl productService;
 
     @GetMapping(path = "")
     public String showProducts(Model model) {
@@ -52,5 +53,18 @@ public class ProductController {
         return "redirect:/products";
     }
 
+    @GetMapping(path = "/edit/{idProduct}")
+    public String goToViewEditProduct(@PathVariable Integer idProduct, Model model) {
+        Product productBD = productService.getProduct(idProduct).get();
+        LOGGER.info("Editing product of the view: {}", productBD);
+        model.addAttribute("product", productBD);
+        return "products/edit";
+    }
 
+    @PostMapping(path = "/update")
+    public String updateProduct(Product product) {
+        LOGGER.info("Updating product of the view: {}", product);
+        productService.updateProduct(product);
+        return "redirect:/products";
+    }
 }
