5178da28022689893d862620123f880776352674
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-05-23 11:02:35 -0500

    metodo guardar y logger para el controller de producto

--- src/main/java/com/isai/demoecommerce/app/controller/ProductController.java
+++ src/main/java/com/isai/demoecommerce/app/controller/ProductController.java
@@ -1,12 +1,18 @@
 package com.isai.demoecommerce.app.controller;
 
+import com.isai.demoecommerce.app.model.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(path = "/products")
 public class ProductController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
 
     @GetMapping(path = "")
     public String showProducts() {
@@ -14,8 +20,15 @@ public class ProductController {
     }
 
     @GetMapping(path = "/createProduct")
-    public String goToViewCreateProduct(){
+    public String goToViewCreateProduct() {
         return "products/create";
+    }
+
+    @PostMapping(path = "/registerProduct")
+    public String registerProduct(
+            Product product) {
+        LOGGER.info("Registering product of the view: {}", product);
+        return "redirect:/products";
     }
 
 }
